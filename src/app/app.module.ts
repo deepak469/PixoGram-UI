5bb372bd10b70a0e5e6993315a0d1b149a24f0bb
edward-whittle <edwardwh@au1.ibm.com>
2019-03-16 13:30:52 +1100

    Image upload skeleton implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,14 @@ import { AdminHomeComponent } from './admin/adminhome.component';
 import { UserHomeComponent } from './user/userhome.component';
 import { LogoutComponent } from './home/logout.component';
 import { UploadComponent } from './user/upload.component';
+import { UserMediaComponent } from './user/usermedia.component';
+import { FileUploadModule, FileSelectDirective } from 'ng2-file-upload';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 export const routes: Routes = [
@@ -22,6 +30,7 @@ export const routes: Routes = [
   { path: 'adminhome', component: AdminHomeComponent },
   { path: 'userhome', component: UserHomeComponent },
   { path: 'upload', component: UploadComponent },
+  { path: 'media', component: UserMediaComponent },
 ];
 
 @NgModule({
@@ -34,11 +43,20 @@ export const routes: Routes = [
     AdminHomeComponent,
     UserHomeComponent,
     UploadComponent,
+    UserMediaComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     RouterModule.forRoot(routes, { onSameUrlNavigation: 'reload' }),
+    FileUploadModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatIconModule,
+    MatCardModule,
+    BrowserAnimationsModule,
   ],
   providers: [ImageService],
   bootstrap: [AppComponent]
---
+++ src/app/user/usermedia.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-usermedia',
+  templateUrl: './usermedia.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserMediaComponent implements OnInit {
+  title = 'UserMedia';
+
+  ngOnInit()
+  {
+    //Get photo
+  }
+}
