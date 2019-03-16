d10cc9561ec6ce4df80f2c3a1e3794eea4783db3
edward-whittle <edwardwh@au1.ibm.com>
2019-03-16 14:23:14 +1100

    Data from http post visible and removed unnecessary form fields

--- src/app/user/upload.component.ts
+++ src/app/user/upload.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {FileUploader} from "ng2-file-upload";
-import {Observable} from "rxjs";
-import {HttpClient} from "@angular/common/http";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { FileUploader } from "ng2-file-upload";
+import { Observable } from "rxjs";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 
 @Component({
   selector: 'app-file-upload',
@@ -14,40 +14,52 @@ export class UploadComponent implements OnInit {
   uploadForm: FormGroup;
   uploadFileUrl = 'http://localhost:8924/api/uploadFile';
 
-  public uploader:FileUploader = new FileUploader({
+  public uploader: FileUploader = new FileUploader({
     isHTML5: true
   });
   title: string = 'Angular File Upload';
-  constructor(private fb: FormBuilder, private http: HttpClient ) { }
+  constructor(private fb: FormBuilder, private http: HttpClient) { }
 
-  uploadSubmit(){
-        for (let i = 0; i < this.uploader.queue.length; i++) {
-          let fileItem = this.uploader.queue[i]._file;
-          if(fileItem.size > 10000000){
-            alert("Each File should be less than 10 MB of size.");
-            return;
-          }
-        }
-        for (let j = 0; j < this.uploader.queue.length; j++) {
-          let data = new FormData();
-          let fileItem = this.uploader.queue[j]._file;
-          console.log(fileItem.name);
-          data.append('file', fileItem);
-          data.append('fileSeq', 'seq'+j);
-          data.append( 'dataType', this.uploadForm.controls.type.value);
-          this.uploadFile(data).subscribe(data => alert(data.message));
-        }
-        this.uploader.clearQueue();
+  uploadSubmit() {
+    for (let i = 0; i < this.uploader.queue.length; i++) {
+      let fileItem = this.uploader.queue[i]._file;
+      if (fileItem.size > 10000000) {
+        alert("Each File should be less than 10 MB of size.");
+        return;
+      }
+    }
+    for (let j = 0; j < this.uploader.queue.length; j++) {
+      let data = new FormData();
+      let fileItem = this.uploader.queue[j]._file;
+      console.log(fileItem.name);
+      data.append('file', fileItem);
+      data.append('fileSeq', 'seq' + j);
+      data.append('dataType', this.uploadForm.controls.type.value);
+      this.uploadFile(data);
+    }
+    this.uploader.clearQueue();
   }
 
-  uploadFile(data: FormData): Observable<any> {
-    return this.http.post(this.uploadFileUrl, data);
+  uploadFile(data: FormData) {
+    this.http.post(this.uploadFileUrl, data).subscribe(data => {
+      console.log(data);
+    }
+      ,
+      (err: HttpErrorResponse) => {
+        if (err.error instanceof Error) {
+          console.log('Client-side error occured.');
+        } else {
+          console.log(err.message);
+          console.log('Server-side error occured.');
+        }
+      }
+    )
   }
 
   ngOnInit() {
     this.uploadForm = this.fb.group({
       document: [null, null],
-      type:  [null, Validators.compose([Validators.required])]
+      type: [null, Validators.compose([Validators.required])]
     });
   }
 
